cd13fbb4014ff7491e43006bf2a33582e0c70112
derrickkilimo <derrick.kilimo@student.moringaschool.com>
2023-10-22 17:58:48 +0300

    werkzeug

--- server/werkzeug_app.py
+++ server/werkzeug_app.py
@@ -1,1 +1,12 @@
 #!/usr/bin/env python3
+from werkzeug.wrappers import Request, Response
+from werkzeug.serving import run_simple
+
+@Request.application
+def application(request):
+    print(f'This web server is running at {request.remote_addr}')
+    response = Response('A WSGI generated this response!')
+    return response
+
+if __name__ == '__main__':
+    run_simple('localhost', 5555, application)
